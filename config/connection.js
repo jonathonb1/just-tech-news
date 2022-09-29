cd3484770b2058f804a1268df6ea5d7cc7f9677b
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-09-28 20:59:24 -0700

    final commit

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our DB, pass in your MySQL info for Username and Password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
 const sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
 
+}
 
 module.exports = sequelize;
