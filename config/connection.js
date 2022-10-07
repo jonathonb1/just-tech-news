b2c8c852b5a9d4790aeaaaca4c4e5cb1ac8d1e13
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-10-07 14:30:17 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,24 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our DB, pass in your MySQL info for Username and Password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // create connection to our DB, pass in your MySQL info for Username and Password
+sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
+}
+// // create connection to our DB, pass in your MySQL info for Username and Password
+// sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
 
 module.exports = sequelize;
