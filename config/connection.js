261275caeac2316b4550781bc7a23a8821a945a6
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-10-07 14:49:02 -0700

    final module commit

--- config/connection.js
+++ config/connection.js
@@ -14,12 +14,7 @@ sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
     port: 3306
 });
 }
-// // create connection to our DB, pass in your MySQL info for Username and Password
-// sequelize = new Sequelize('just_tech_news_db', 'root', 'rootpassword', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
+
 
 
 module.exports = sequelize;
