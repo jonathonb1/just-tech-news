2f5c687c3c838697d82492f5bec474c2142d6cfe
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-10-10 16:51:28 -0700

    added login/logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,6 +19,7 @@ const router = require('express').Router();
 // });
 
 router.get('/', (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -55,6 +56,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
